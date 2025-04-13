ab3608f439e048dbe98630c3bf5c3d2747bdbe5e
Ольга Ананьева <olgaananeva@MacBook-Pro-Olga.local>
2025-04-13 20:15:14 +0900

    feat: add NextAuth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Providers } from './providers'
 
 const inter = Inter({ 
   subsets: ['latin', 'cyrillic'],
@@ -20,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru" className={inter.variable}>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 } 
